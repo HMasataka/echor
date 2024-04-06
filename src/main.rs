7144863502966301a4e990e60c4a4eb9ab78ddf2
Masataka Hisasue <masataka.hisasue@gmail.com>
2024-04-07 00:17:51 +0900

    Feat clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use clap::App;
+
 fn main() {
-    println!("{:?}", std::env::args());
+    let _matches = App::new("echor")
+        .version("1.0.0")
+        .author("example@email.com")
+        .about("Rust echo")
+        .get_matches();
 }
